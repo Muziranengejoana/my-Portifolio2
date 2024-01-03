65912f0ab120da8507874ca6c4c8936919c59f22
joana <muziranengejoana@gmail.com>
2024-01-03 06:54:03 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// /* ===================== typing animation ========================= */
+// document.addEventListener(DOMco)
+// var typed = new Typed(".typing",{
+//     Strings: ["Web Developer","Web Designer",],
+//     typeSpeed: 100,
+//     BackSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// })
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".words", {
+        strings: ["MADE UP", "SCHOOL"],
+        typeSpeed: 50,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
